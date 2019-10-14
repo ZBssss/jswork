932202799564dbed2bb9353950ebdd12ccc1fe4f
ZBssss <ZBssss@gdmec.edu.cn>
2019-10-14 22:10:23 +0800

    fourth commit

--- work5/work5.js
+++ work5/work5.js
@@ -1,9 +1,8 @@
 //九九乘法空表
-
 let str = '<table border="1">'
 for(let a=1;a<10;++a){
     str +='<tr>'
-    for(var b=1;b<=a;++b){
+    for(var b=1;b<=10;++b){
         //拼接单元格
         str += '<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<td>'
         }
@@ -24,3 +23,15 @@ for(let i=9;i>0;--i){
 }
 str += '</table>'
 document.getElementById('table2').innerHTML = str
+
+//倒九九乘法表
+str = '<table border="1">'
+for(let c=9;c>0;--c){
+    str +='<tr>'
+    for(var d=c;d>0;--d){
+		str += '<td>' + d + '*' +c + '=' +(d*c)+'<td>'
+    }
+    str +='</tr>'
+}
+str += '</table>'
+document.getElementById('table3').innerHTML = str
